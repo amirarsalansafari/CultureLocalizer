ad065586129e0b7b7e4ba62078400f3a93b9040c
Arsalan <Arsalan@DESKTOP-NOHCTTI>
2025-01-11 16:12:47 +0330

    Add project files.

---
+++ Culture/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace Culture.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly IStringLocalizer<HomeController> _localizer;
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult HelloWorld()
+        {
+            var message = _localizer["HelloWorldMessage"];
+            return Ok(new { message });
+        }
+    }
+}
---
+++ Culture/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Culture
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // اضافه کردن تنظیمات Localization
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            // اضافه کردن کنترلرها و تنظیمات Localization
+            builder.Services.AddControllers()
+                .AddDataAnnotationsLocalization()
+                .AddViewLocalization();
+
+            // اضافه کردن Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            var supportedCultures = new[] { "fa-IR", "en-US", "ar-SA" }; // زبان‌های پشتیبانی شده
+            var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+
+            // فعال کردن Middleware برای Localization
+            app.UseRequestLocalization(localizationOptions);
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
